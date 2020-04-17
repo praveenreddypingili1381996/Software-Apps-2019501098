736dc4029711f0f2a1ade3fa0d540d523bcfbfa9
praveenreddypingili1381996 <praveenreddypingili123@msitprogram.net>
2020-04-17 10:19:11 +0530

    task1

--- project1/application.py
+++ project1/application.py
@@ -1,9 +1,10 @@
 import os
-
+import datetime
 from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from models import *
 
 app = Flask(__name__)
 
@@ -17,9 +18,15 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+#engine = create_engine(os.getenv("DATABASE_URL"))
+#db = scoped_session(sessionmaker(bind=engine))
 
+#Tell Flask what SQLAlchemy database to use.
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+#Link the Flask app with the database
+db.init_app(app)
 
 @app.route("/")
 def index():
@@ -31,9 +38,19 @@ def register():
     if (request.method == "POST"):
        
         email = request.form.get("email")
-        print(email)
+        #print(email)
         password = request.form.get("psw")
-        print(password)
+        #print(password)
         return render_template("email.html",name=email)
     return render_template("register.html") 
-    
+
+
+
+def main():
+    app.app_context().push()
+    db.create_all()    
+
+
+
+if __name__ == "__main__":
+    main()
