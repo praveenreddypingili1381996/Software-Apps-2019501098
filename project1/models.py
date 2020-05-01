867216e4a149bc7379eb88fb33ce8a93891c0d5e
praveenreddypingili1381996 <praveenreddypingili123@msitprogram.net>
2020-05-01 18:00:59 +0530

    finished

--- project1/models.py
+++ project1/models.py
@@ -10,10 +10,10 @@ class Data(db.Model):
 
 
     class Book(db.Model):
-    __tablename__ = "books"
-    id = db.Column(db.Integer, primary_key=True)
-    isbn = db.Column(db.String, nullable=False)
-    title = db.Column(db.String, nullable=False)
-    author = db.Column(db.String, nullable=False)
-    pub_year = db.Column(db.Integer, nullable=False)
-    revs = db.Column(db.String, nullable=True)
+        __tablename__ = "books"
+        id = db.Column(db.Integer, primary_key=True)
+        isbn = db.Column(db.String, nullable=False)
+        title = db.Column(db.String, nullable=False)
+        author = db.Column(db.String, nullable=False)
+        pub_year = db.Column(db.Integer, nullable=False)
+        revs = db.Column(db.String, nullable=True)
